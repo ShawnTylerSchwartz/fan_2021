1dc81941fdecf0ca13032951db2854aad2df8333
juliarobink <Julia.Kearley@stonybrook.edu>
2021-05-13 13:48:01 -0400

    Added catch trials into encoding trials

--- Experiment/TrialTypes/JOL1/trial.php
+++ Experiment/TrialTypes/JOL1/trial.php
@@ -1,6 +1,6 @@
 <?php
-	$compTime = 8;					// time in seconds to use for 'computer' timing
-    if ($text === '') { $text = 'How likely are you to correctly recall this item on a later test?|Type your response on a scale from 0-100.'; }
+	$compTime = 7;					// time in seconds to use for 'computer' timing
+    if ($text === '') { $text = 'Please type the letter "b"'; }
     $texts = explode('|', $text);
     $mainText = array_shift($texts);
 ?>
@@ -15,6 +15,6 @@
     </div>
 
     <div class="textcenter">
-        <input name="JOL" type="text" value="" autocomplete="off" class="forceNumeric"/>
+        <input name="JOL" type="text" value="" autocomplete="off" class="textcenter"/>
         <input class="button"  id="FormSubmitButton" type="submit" value="Submit"   />
     </div>
---
+++ Experiment/TrialTypes/JOL2/trial.php
@@ -0,0 +1,20 @@
+<?php
+	$compTime = 7;					// time in seconds to use for 'computer' timing
+    if ($text === '') { $text = 'Please type the letter "f"'; }
+    $texts = explode('|', $text);
+    $mainText = array_shift($texts);
+?>
+
+    <div class="textcenter">
+        <h3><?php echo $mainText; ?></h3>
+        <?php
+            foreach ($texts as $t) {
+                echo '<p>' . $t . '</p>';
+            }
+        ?>
+    </div>
+
+    <div class="textcenter">
+        <input name="JOL" type="text" value="" autocomplete="off" class="textcenter"/>
+        <input class="button"  id="FormSubmitButton" type="submit" value="Submit"   />
+    </div>
